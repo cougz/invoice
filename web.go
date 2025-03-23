626b7c83ebc4c7cfc86b462c39ad9b42ff5b6c42
cougz <cougz@github.com>
2025-03-23 22:57:37 +0000

    Bootstrap & Dark mode fixes

--- web.go
+++ web.go
@@ -54,6 +54,7 @@ var HTMLTemplates = map[string]string{
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Invoice Generator</title>
     <link href="/static/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="container">
